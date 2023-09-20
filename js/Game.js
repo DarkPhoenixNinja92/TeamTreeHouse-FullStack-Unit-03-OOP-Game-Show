ea4400374cd62e2f6d0c89accdafee03b13e922d
Shaun <scpour@gmail.com>
2023-09-20 12:50:27 -0400

    Set up basic starting state for enabling game display and picking a phrase

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+class Game {
+    constructor() {
+    this.missed = 0;
+    this.phrases = [
+        new Phrase("Beauty is in the eye of the beholder"),
+        new Phrase("Spill the beans"),
+        new Phrase("Break a leg"),
+        new Phrase("As right as rain"),
+        new Phrase("Once in a blue moon")
+    ];
+    this.activePhrase = null;
+    }
+    getRandomPhrase() {
+        const indexRand = Math.floor(Math.random() * this.phrases.length);
+        const phraseRand = this.phrases[indexRand];
+        return phraseRand;
+    };
+    startGame() {
+        const overlay = document.getElementById('overlay');
+        const button = document.getElementById('btn__reset');
+        
+        button.addEventListener('click', () => {
+            overlay.style.display = 'none';
+        });
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay() {
+        const phraseUlParent = document.getElementById('phrase');
+        const ul = phraseUlParent.firstElementChild ;
+
+        for (let i = 0; i < this.phrase.length; i++){
+            const letter = document.createElement('li');
+            if(this.phrase[i] === ' '){
+                letter.classList.add('space');
+            }else{
+                letter.classList.add('letter');
+                letter.classList.add(`${this.phrase[i]}`);
+                letter.classList.add('hide');
+                letter.textContent = this.phrase[i];
+            }
+            ul.appendChild(letter);
+        }
+    };
+}
